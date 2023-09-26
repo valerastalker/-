5ca1022b89a0cca526efabbdb81592a76900faa0
valerastalker <145466687+valerastalker@users.noreply.github.com>
2023-09-26 17:48:05 +0300

    Add files via upload

---
+++ muromets.py
@@ -0,0 +1,23 @@
+name = ( ' илья муромец )
+
+y_left = False
+y_center = False
+y_right = False
+
+      ile way_left == False or way_center == False or way_rite  == False :
+         
+print ( name ,  "  приехал к камню , на нем
+print  ( " налево пойдешь - убитым быть " )
+print   ( "прямо пойдешь - женатым будешь " )
+print  ( " налево пойдешь - богатым быть " )
+
+way = input ( " в какую сторону пойти " )
+if way == " налево " :
+  print = ( name " убит " )
+  way_left = True
+elif way ==" прямо "
+   print = ( name " женат " )
+   way_center = True
+elif way ==" прямо "
+   print = ( name " богат " )
+   way_right = True
