4ee7879b7d68c7954f1c7836dec28428fbd36397
valerastalker <145466687+valerastalker@users.noreply.github.com>
2023-10-03 17:48:48 +0300

    Add files via upload

---
+++ kosti.py
@@ -0,0 +1,24 @@
+from ramdom inport randimit
+
+money = 10
+
+while true :
+ input ( ' нажмите ентер чтобы нпчать игру' )
+ pleyer_1 = randint ( 2,12 )
+ pleyer_2 = randint( 2,12 )
+
+pleyer_1 = randint ( 2,12 )
+pleyer_2 = randint( 2,12 )
+print ( num )
+
+print ( ' игрок выбросил ' player_1 )
+print ( ' комп выбросил  ' player_2 )
+
+if player_1 > player 2
+  print ( ' вы победили ' money == money + 1 )
+elif player_1 < player 2
+ print ('компьютер победил' money == money -1 )
+elif:
+ print ( ' ничья ' )
+elif money = 0
+ print (' проиграл ')
